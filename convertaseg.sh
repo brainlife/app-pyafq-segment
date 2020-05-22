90d2d3ef99c23eebc86b9d02a2a4ea0a1a16d0fe
Brad Caron <bacaron@iu.edu>
2020-05-21 21:17:50 -0400

    Update convertaseg.sh

--- convertaseg.sh
+++ convertaseg.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 freesurfer=`jq -r '.freesurfer' config.json`
+dwi=`jq -r '.dwi' config.json`
+
+export SUBJECTS_DIR=./
 
 mri_convert $freesurfer/mri/aparc+aseg.mgz aparc+aseg.nii.gz
+mri_vol2vol --mov aparc+aseg.nii.gz --targ ${dwi} --regheader --interp nearest --o aparc+aseg.nii.gz 
 mri_convert $freesurfer/mri/aparc.a2009s+aseg.mgz aparc.a2009s+aseg.nii.gz
+mri_vol2vol --mov aparc.a2009s+aseg.nii.gz --targ ${dwi} --regheader --interp nearest --o aparc.a2009s+aseg.nii.gz 
+
