1374dc0cc2584abef21cef24a2812a1db270e119
Brad Caron <bacaron@iu.edu>
2020-10-27 10:24:11 -0400

    reason: final updates

--- segment-afq.py
+++ segment-afq.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""Code adapted from pyAFQ tutorial (https://github.com/yeatmanlab/pyAFQ/blob/master/examples/plot_tract_profile.py).
+   Author: Ariel Rokem (github.com/arokem)
+   Adapted for brainlife.io by Brad Caron (github.com/bacaron)
+"""
+
 import os.path as os
 import json
 import numpy as np
@@ -13,11 +18,6 @@ import dipy.core.gradients as dpg
 import scipy.io as sio
 from matplotlib import cm
 
-# make output directories
-#os.mkdir("wmc")
-#os.mkdir("wmc/tracts")
-#os.mkdir("wmc/surfaces")
-
 # open configurable inputs
 with open('config.json') as config_f:
     config = json.load(config_f)
@@ -25,19 +25,32 @@ with open('config.json') as config_f:
     bvals = config["bvals"]
     bvecs = config["bvecs"]
     track = config["track"]
+    affine = config["affine"]
 
 # load dwi data and generate gradient table
 dwi_img = nib.load(dwi)
 gtab = dpg.gradient_table(bvals,bvecs,b0_threshold=50)
 
 # load MNI template and syn register dwi data to MNI
-#MNI_T2_img = dpd.read_mni_template()
 MNI_T2_img = nib.load('/templateflow/tpl-MNI152NLin2009cAsym/tpl-MNI152NLin2009cAsym_res-01_T2w.nii.gz')
-warped_hardi, mapping = reg.syn_register_dwi(dwi, gtab)
+
+# run affine registration if requested
+if affine == 'true':
+    
+    # create mean b0 image
+    b0 = np.mean(dwi_img.get_fdata()[..., gtab.b0s_mask], -1)
+    
+    # register DWI to MNI using affine
+    , prealign = reg.affine_registration(b0,MNI_T2_img.get_fdata(),dwi_img.affine,MNI_T2_img.affine)
+
+# run nonlinear (SyN) registration
+if affine == "":
+    warped_hardi, mapping = reg.syn_register_dwi(dwi, gtab)
+else:
+    warped_hardi, mapping = reg.syn_register_dwi(dwi, gtab, prealign=prealign)
 
 # load tractogram
 tg = load_tractogram(track,dwi_img,bbox_valid_check=False)
-#tg_acpc = transform_streamlines(tg.streamlines,dwi_img.get_affine())
 
 # download and load waypoint ROIs and make bundle dictionary
 bundles = api.make_bundle_dict(resample_to=MNI_T2_img)
@@ -48,12 +61,12 @@ print(f"Space before segmentation: {tg.space}")
 # initialize segmentation and segment major fiber groups
 print("running AFQ segmentation")
 segmentation = seg.Segmentation(return_idx=True)
-segmentation.segment(bundles,tg,fdata=dwi,fbval=bvals,fbvec=bvecs,mapping=mapping,reg_template=MNI_T2_img)
+segmentation.segment(bundles,tg,fdata=dwi,fbval=bvals,fbvec=bvecs,mapping=mapping,reg_template=MNI_T2_img,reset_tg_space=True)
 
 print(f"Space after segmentation: {tg.space}")
 
 # re-load tractogram in RASMM space since it was warped to the VOX space during segmentation
-tg = load_tractogram(track,dwi_img,bbox_valid_check=False)
+#tg = load_tractogram(track,dwi_img,bbox_valid_check=False)
 
 # generate classification structure and tracts.json
 names = np.array(bundle_names,dtype=object)
@@ -92,3 +105,16 @@ print("saving classification.mat")
 sio.savemat('wmc/classification.mat', { "classification": {"names": names, "index": streamline_index }})
 
 print("AFQ segmentation complete")
+
+##########################################################################
+# References:
+# -------------------------
+# .. [Yeatman2012] Jason D Yeatman, Robert F Dougherty, Nathaniel J Myall,
+#                  Brian A Wandell, Heidi M Feldman, "Tract profiles of
+#                  white matter properties: automating fiber-tract
+#                  quantification", PloS One, 7: e49790
+#
+# .. [Yeatman2014] Jason D Yeatman, Brian A Wandell, Aviv Mezer Feldman,
+#                  "Lifespan maturation and degeneration of human brain white
+#                  matter", Nature Communications 5: 4932
+
